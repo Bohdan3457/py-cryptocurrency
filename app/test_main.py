5f01005947692d751e076115e95b25acdd7c53dd
Bohdan3457 <bohdankhomenko04@gmail.com>
2026-04-05 12:01:33 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from app.main import cryptocurrency_action
+from pytest import MonkeyPatch
+
+
+def test_buy_more_cryptocurrency(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        "app.main.get_exchange_rate_prediction", lambda current_rate: 106)
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+def sell_all_your_cryptocurrency(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        "app.main.get_exchange_rate_prediction", lambda current_rate: 94)
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+def test_do_nothing_upper_border(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        "app.main.get_exchange_rate_prediction", lambda current_rate: 105)
+    assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_do_nothing_lower_border(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        "app.main.get_exchange_rate_prediction", lambda current_rate: 95)
+    assert cryptocurrency_action(100) == "Do nothing"
